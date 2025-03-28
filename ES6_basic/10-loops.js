896ccebc9a090aa65d8b71eb382f875818709be5
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-28 23:14:02 +0100

    erreur sur les fichier de config

--- ES6_basic/10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const result = [...array];
-
-  for (const [idx, value] of result.entries()) {
-    result[idx] = appendString + value;
-  }
-
-  return result;
-}
