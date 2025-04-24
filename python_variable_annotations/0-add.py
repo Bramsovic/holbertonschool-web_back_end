4d89d3149b11695a7d453c426459b1f08841a60f
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-04-24 14:25:17 +0200

    fix: pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,14 +3,15 @@
 This module provides a simple addition function with type annotations.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Adds two floating-point numbers and returns the result as a float.
-    
+
     Parameters:
     a (float): The first number.
     b (float): The second number.
-    
+
     Returns:
     float: The sum of a and b.
     """
