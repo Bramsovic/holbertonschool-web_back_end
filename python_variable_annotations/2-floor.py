129f68423a9a7a13a8c4686c9411d0f4e8faf879
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-04-24 15:22:38 +0200

    Add floor() function for float conversion

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns
+the floor of a floating-point number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given float as an integer.
+    Parameters:
+    n (float): The number to floor.
+    Returns:
+    int: The largest integer less than or equal to n.
+    """
+    return int(n)
