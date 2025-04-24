cd9d02ec26fbfe06513f53ed651b54caadf5a393
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-04-24 14:19:43 +0200

    Add float addition function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple addition function with type annotations.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns the result as a float.
+    
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+    
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
