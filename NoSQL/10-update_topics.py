0973444453ce45e3d96f694f9958a7bb26bead2e
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-05-01 19:44:48 +0200

    Add function to update school topics in MongoDB

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Update school topics in MongoDB collection."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update topics of a school document by name."""
+    update_result = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return update_result.modified_count
