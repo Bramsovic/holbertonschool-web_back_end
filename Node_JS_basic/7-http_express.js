ea2f2496db00faf09a9f4e1b41003ea619d796a5
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-06-05 19:54:33 +0200

    implement Express server with student data endpoints

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      for (const line of students) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+
+      let result = `Number of students: ${students.length}`;
+      for (const field in fields) {
+        if (Object.hasOwn(fields, field)) {
+          result += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        }
+      }
+
+      return result;
+    });
+}
+
+// Route /
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Route /students
+app.get('/students', (req, res) => {
+  const path = process.argv[2];
+
+  countStudents(path)
+    .then((data) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch(() => {
+      res.set('Content-Type', 'text/plain');
+      res.status(500).send('Cannot load the database');
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
