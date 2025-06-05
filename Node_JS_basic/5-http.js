a41d0fd5bb6957bd003e3332db132077865a4b3d
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-06-05 18:35:40 +0200

    implement Node.js server with / and /students routes

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      for (const line of students) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+
+      let result = `Number of students: ${students.length}`;
+      for (const field in fields) {
+        if (Object.hasOwn(fields, field)) {
+          result += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        }
+      }
+
+      return result;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
