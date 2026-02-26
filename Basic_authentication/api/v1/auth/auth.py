739dec63c62e4231ae5b6449062f015fc50b2779
« bramsovic» <haddad.brahim.59000@gmail.com>
2026-02-26 10:45:24 +0100

    Update Auth.require_auth with excluded paths support

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,23 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return whether authentication is required for a path."""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path is None:
+                continue
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return the authorization header from a Flask request."""
