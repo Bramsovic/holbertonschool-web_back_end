7e79c927b6878edd0582bb4604332b8f44b4244f
« bramsovic» <haddad.brahim.59000@gmail.com>
2026-02-26 11:27:04 +0100

    Add BasicAuth class and AUTH_TYPE basic_auth support

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Define the BasicAuth authentication class."""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Represent the basic authentication mechanism."""
+
+    pass
