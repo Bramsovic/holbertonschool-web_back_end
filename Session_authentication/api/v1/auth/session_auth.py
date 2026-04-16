398649661a5c558223581f47c1597c8f4f55c12e
« bramsovic» <haddad.brahim.59000@gmail.com>
2026-04-16 22:12:24 +0200

    Add users me endpoint and session auth scaffold

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Define the SessionAuth authentication class."""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Represent an empty session authentication mechanism."""
+
+    pass
