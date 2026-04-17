50fb6ef4a8c5495e86c29eb35091852dbfeed357
« bramsovic» <haddad.brahim.59000@gmail.com>
2026-04-17 15:11:47 +0200

    Implement session ID creation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
             return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID for an existing user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
