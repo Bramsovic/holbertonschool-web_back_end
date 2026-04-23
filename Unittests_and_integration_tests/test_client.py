ca6d9f3aa3f6cb3f32e9cfcbcba4ddcc8e4945a6
« bramsovic» <haddad.brahim.59000@gmail.com>
2026-04-23 10:12:46 +0200

    Fix has_license test signature for checker

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -69,9 +69,10 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_has_license(
-        self, repo: dict, license_key: str, expected: bool
-    ) -> None:
+    def test_has_license(self,
+                         repo: dict,
+                         license_key: str,
+                         expected: bool) -> None:
         """Check that has_license returns the expected boolean."""
         self.assertEqual(
             GithubOrgClient.has_license(repo, license_key), expected
