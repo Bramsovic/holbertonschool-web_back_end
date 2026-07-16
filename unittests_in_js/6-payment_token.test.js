d4de8ee6618969eefbdd4d4761ccbcfe49b5103b
Brahim Hdd <haddad.brahim.59000@gmail.com>
2026-07-16 21:25:13 +0200

    Add async payment token test

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('returns a successful API response when success is true', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        assert.deepStrictEqual(response, { data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
